81954ff10db17d5b1c88421961326a41a76c7bdb
Elmer Raimundo dos Santos <eelmer226@gmail.com>
2020-06-12 16:23:19 -0300

    Arquivos

---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+let email = 'eelmer226@gmail.com';
+
+console.log(email);
+console.log('O seu e-mail é: ' + email);
+console.log(`O seu e-mail é: ${email}`);
+
+
+document.getElementById('btn-submit').addEventListener('click', e => {
+
+    console.log('O botão foi clicado!');
+
+});
+
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+
+    console.log('O mouse está sobre o formulário.');
+
+});
+
+document.querySelector('#form-login').addEventListener('mouseleave', e=> {
+
+    console.log('O mouse está fora do formulário.');
+
+});
+
+document.querySelector('#form-login').addEventListener('submit', e=>{
+
+    e.preventDefault();
+
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    let json = {
+        email,
+        password
+    };
+
+    if (!json.email) {
+
+        console.error("O campo e-mail deve ser preenchido!");
+
+    } else if (!json.password) {
+
+        console.error("O campo password deve ser preenchido!");
+
+    } else {
+
+        console.info("Dados validados com sucesso!");
+
+    }
+
+});
